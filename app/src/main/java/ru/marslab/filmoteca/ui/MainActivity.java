917b14c07c78b310ce3654b23c80983f3cadc2b1
Sergey Aristov <aristov775@gmail.com>
2021-07-23 18:34:22 +0300

    Создано основное активити MainActivity

---
+++ app/src/main/java/ru/marslab/filmoteca/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.marslab.filmoteca.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.marslab.filmoteca.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
