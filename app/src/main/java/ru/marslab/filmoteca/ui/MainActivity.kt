35dc2d14cb0dc7c8c004d086cea5ae26b5709a0b
Sergey Aristov <aristov775@gmail.com>
2021-07-23 18:44:46 +0300

    4. Добавлена кнопка и на нее повешен тост для отработки clickListener

--- app/src/main/java/ru/marslab/filmoteca/ui/MainActivity.kt
+++ app/src/main/java/ru/marslab/filmoteca/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.marslab.filmoteca.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ru.marslab.filmoteca.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firstButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        firstButton = findViewById(R.id.first_button)
+        firstButton.setOnClickListener {
+            Toast.makeText(this, getString(R.string.first_button), Toast.LENGTH_SHORT).show()
+        }
     }
 }
