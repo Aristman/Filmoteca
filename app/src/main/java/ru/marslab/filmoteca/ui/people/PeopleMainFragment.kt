8ce9ec970dd52d624599af8e675457c3ec02a741
Sergey Aristov <aristov775@gmail.com>
2021-07-29 19:20:05 +0300

    Добавлен фрагмент для отображения людей с вьюмоделью

---
+++ app/src/main/java/ru/marslab/filmoteca/ui/people/PeopleMainFragment.kt
@@ -0,0 +1,33 @@
+package ru.marslab.filmoteca.ui.people
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import ru.marslab.filmoteca.databinding.FragmentPeopleMainBinding
+
+class PeopleMainFragment : Fragment() {
+    private var _binding: FragmentPeopleMainBinding? = null
+    private val binding: FragmentPeopleMainBinding
+        get() = _binding!!
+    private val peopleMainViewModel by viewModels<PeopleMainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPeopleMainBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
