b951911d06eaf169439299f7e08c40d27779ac65
Sergey Aristov <aristov775@gmail.com>
2021-08-08 19:48:37 +0300

    Исправление замечаний по предыдущим домашним заданиям

--- app/src/main/java/ru/marslab/filmoteca/domain/di/DataModule.kt
+++ app/src/main/java/ru/marslab/filmoteca/domain/di/DataModule.kt
@@ -1,10 +1,13 @@
 package ru.marslab.filmoteca.domain.di
 
+import android.app.Application
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import ru.marslab.filmoteca.App
 import ru.marslab.filmoteca.data.RepositoryMocaImpl
 import ru.marslab.filmoteca.domain.Repository
 
@@ -14,6 +17,9 @@ object DataModule {
 
     @ViewModelScoped
     @Provides
-    fun provideRepository() : Repository =
-        RepositoryMocaImpl()
+    fun provideRepository(): Repository = RepositoryMocaImpl()
+
+    @ViewModelScoped
+    @Provides
+    fun provideContext(app: Application): Context = app.baseContext
 }
--- app/src/main/java/ru/marslab/filmoteca/ui/guest/GuestViewModel.kt
+++ app/src/main/java/ru/marslab/filmoteca/ui/guest/GuestViewModel.kt
@@ -1,5 +1,6 @@
 package ru.marslab.filmoteca.ui.guest
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,13 +9,18 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import ru.marslab.filmoteca.R
 import ru.marslab.filmoteca.domain.Repository
+import ru.marslab.filmoteca.ui.util.Constants.LOAD_DELAY
 import ru.marslab.filmoteca.ui.util.ViewState
 import javax.inject.Inject
 import kotlin.random.Random
 
 @HiltViewModel
-class GuestViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+class GuestViewModel @Inject constructor(
+    private val repository: Repository,
+    private val context: Context
+) : ViewModel() {
     private var _ratedMovies: MutableLiveData<ViewState> = MutableLiveData()
 
     val ratedMovies: LiveData<ViewState>
@@ -24,14 +30,14 @@ class GuestViewModel @Inject constructor(private val repository: Repository) : V
     fun getRatedMoviesList() {
         _ratedMovies.value = ViewState.Loading
         viewModelScope.launch(Dispatchers.IO) {
-            delay(2000L)
+            delay(LOAD_DELAY)
             if (Random.nextBoolean()) {
                 _ratedMovies.postValue(
                     ViewState.Successful(repository.getRatedMovies())
                 )
             } else {
                 _ratedMovies.postValue(
-                    ViewState.LoadError("Ошибка загрузки списка фильмов")
+                    ViewState.LoadError(context.getString(R.string.load_list_movies_error))
                 )
             }
         }
