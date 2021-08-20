34e58b60f61b504d7e163f9fde6a5b098580107a
Sergey Aristov <aristov775@gmail.com>
2021-08-20 14:58:39 +0300

    Реализация сохранения настройки Adult

--- app/src/main/java/ru/marslab/filmoteca/ui/settings/SettingsFragment.kt
+++ app/src/main/java/ru/marslab/filmoteca/ui/settings/SettingsFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import ru.marslab.filmoteca.R
 import ru.marslab.filmoteca.databinding.FragmentSettingsBinding
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding: FragmentSettingsBinding
         get() = checkNotNull(_binding) { getString(R.string.binding_not_init) }
+    private val settingsViewModel by viewModels<SettingsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,26 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        binding.run {
+            settingAdult.setOnCheckedChangeListener { _, isChecked ->
+                settingsViewModel.adult = isChecked
+            }
+        }
+    }
+
+    private fun initView() {
+        binding.run {
+            settingAdult.isChecked = settingsViewModel.adult
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ru/marslab/filmoteca/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/ru/marslab/filmoteca/ui/settings/SettingsViewModel.kt
@@ -3,12 +3,22 @@ package ru.marslab.filmoteca.ui.settings
 import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.marslab.filmoteca.domain.repository.Store
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
+    private val store: Store
 ) : ViewModel() {
 
+    var adult: Boolean
+        get() = store.adult
+        set(value) {
+            store.adult = value
+            sharedPreferences.edit()
+                .putBoolean(Store.SETTING_ADULT, value)
+                .apply()
+        }
 
 }
