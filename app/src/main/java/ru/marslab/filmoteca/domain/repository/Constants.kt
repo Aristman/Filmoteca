970483ec19b10d78229fa137b8a0ebf94d1155dc
Sergey Aristov <aristov775@gmail.com>
2021-08-27 19:49:10 +0300

    Реализовано получение разрешения на доступ к локации. При начальном пуске идет определение текущей локации и перемещение карты на нее

--- app/src/main/java/ru/marslab/filmoteca/domain/repository/Constants.kt
+++ app/src/main/java/ru/marslab/filmoteca/domain/repository/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_POSTER_URL = "https://image.tmdb.org/t/p/w500"
 
+    const val GPS_REFRESH_PERIOD = 60000L
+    const val GPS_DISTANCE = 100f
+
     const val SETTING_ADULT = "adult"
     const val SETTING_LANGUAGE = "setting_language"
     const val SETTING_TIMEZONE = "setting_region"
--- app/src/main/java/ru/marslab/filmoteca/ui/googlemap/MapsFragment.kt
+++ app/src/main/java/ru/marslab/filmoteca/ui/googlemap/MapsFragment.kt
@@ -1,39 +1,43 @@
 package ru.marslab.filmoteca.ui.googlemap
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.google.android.gms.common.GoogleApiAvailability
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.coroutineScope
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.marslab.filmoteca.R
 import ru.marslab.filmoteca.databinding.FragmentMapsBinding
-import ru.marslab.filmoteca.ui.util.logMessage
+import ru.marslab.filmoteca.domain.repository.Constants
+import ru.marslab.filmoteca.ui.util.showMessage
+import java.io.IOException
+
+private const val REQUEST_CODE = 12
 
 class MapsFragment : Fragment() {
     private var _binding: FragmentMapsBinding? = null
     private val binding: FragmentMapsBinding
         get() = checkNotNull(_binding) { getString(R.string.binding_not_init) }
+    private var googleMap: GoogleMap? = null
+    private var mapLocation: MutableLiveData<LatLng> = MutableLiveData()
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,10 +45,6 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
-        logMessage(
-            GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(requireContext())
-                .toString()
-        )
         return _binding?.root
     }
 
@@ -52,6 +52,131 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment =
             childFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        lifecycle.coroutineScope.launchWhenCreated {
+            googleMap = mapFragment?.awaitMap()
+            googleMap?.moveCamera(CameraUpdateFactory.zoomBy(10f))
+        }
+        checkPermission()
+        initObservers()
+        initListeners()
+    }
+
+    private fun initObservers() {
+        mapLocation.observeForever {
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLng(it))
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getLocation()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.permission_location_dialog_title)
+                    .setMessage(R.string.permission_location_dialog_message)
+                    .setPositiveButton(R.string.ok) { _, _ ->
+                        requestPermission()
+                    }
+                    .setNegativeButton(R.string.cancel) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+            }
+            else -> {
+                requestPermission()
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                getLocation()
+            } else {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.permission_location_dialog_title)
+                    .setMessage(R.string.permission_location_dialog_message)
+                    .setPositiveButton(R.string.ok) { _, _ ->
+                        requestPermission()
+                    }
+                    .setNegativeButton(R.string.cancel) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+            }
+        }
+            .launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults.first() == PackageManager.PERMISSION_GRANTED) {
+                    getLocation()
+                } else {
+                    requireView().showMessage(R.string.permission_location_dialog_message)
+                }
+            }
+        }
+    }
+
+    private fun getLocation() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val locationService =
+                requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationService.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                val provider = locationService.getProvider(LocationManager.GPS_PROVIDER)
+                provider?.let {
+                    locationService.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        Constants.GPS_REFRESH_PERIOD,
+                        Constants.GPS_DISTANCE,
+                        onLocationListener
+                    )
+                }
+            }
+        }
+    }
+
+    private val onLocationListener = LocationListener { location ->
+        val geocoder = Geocoder(requireContext())
+        lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            try {
+                val address = geocoder.getFromLocation(
+                    location.latitude,
+                    location.longitude,
+                    1
+                )
+                mapLocation.postValue(LatLng(location.latitude, location.longitude))
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.addressFindButton.setOnClickListener {
+//            googleMap?.
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
